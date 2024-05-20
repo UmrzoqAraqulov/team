d47a0ddb4c67933886bc0fdbf3ffb74d12a0618d
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-05-20 12:42:19 +0500

    iskandar page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Layout from "./components/layout";
 import About from "./pages/about";
+import Iskandar from "./pages/iskandar/Iskandar";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="iskandar" element={<Iskandar />} />
         </Route>
       </Routes>
     </Fragment>
---
+++ src/pages/iskandar/Iskandar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./iskandar.scss"
+
+const Iskandar = () => {
+  return (
+    <div>
+      <h2 className='iskandar__text'>Iskandar</h2>
+    </div>
+  )
+}
+
+export default Iskandar
