f48960fad2a5a699a63742f349c70e6cd2a0f8d9
LutfulloAsqarov <lutfulloasqarov9@gmail.com>
2024-05-20 12:39:44 +0500

    al

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Layout from "./components/layout";
 import About from "./pages/about";
+import Lutfullo from "./pages/lutfullo/Lutfullo";
 
 function App() {
-  return (
-    <Fragment>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Route>
-      </Routes>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="lutfullo" element={<Lutfullo />} />
+                </Route>
+            </Routes>
+        </Fragment>
+    );
 }
 
 export default App;
